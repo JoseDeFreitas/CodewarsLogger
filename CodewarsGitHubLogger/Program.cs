eb93643fb42db32e758b6a398d0541621b7ed6c6
Jose De Freitas <joselocotote26@gmail.com>
2021-09-14 17:19:57 -0400

    Create .NET console app

---
+++ CodewarsGitHubLogger/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodewarsGitHubLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
