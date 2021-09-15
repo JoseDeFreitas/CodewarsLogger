c7cbe26ca7521f4b2edc9a44019e223d415876c9
Jose De Freitas <joselocotote26@gmail.com>
2021-09-15 12:02:12 -0400

    Include packages to handle async and consume the API

--- CodewarsGitHubLogger/Program.cs
+++ CodewarsGitHubLogger/Program.cs
@@ -1,15 +1,29 @@
 ﻿using System;
 using System.IO;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
 
 namespace CodewarsGitHubLogger
 {
     class Program
     {
-        static string folderPath = "../Katas/";
+        static HttpClient httpClient = new HttpClient();
+        static string codewarsUsername = Environment.GetEnvironmentVariable("CODEWARS_USERNAME");
+        static string userUrl = $"https://www.codewars.com/api/v1/users/{codewarsUsername}";
 
         static void Main(string[] args)
         {
+            string folderPath = "../Katas/";
+
             Directory.CreateDirectory(folderPath);
+
+            Console.WriteLine(GetKatas(0));
+        }
+
+        static async Task<string> GetKatas(int pageNumber)
+        {
+            var response = await httpClient.GetStringAsync($"{userUrl}/code-challenges/completed?page={pageNumber}");
         }
     }
 }
