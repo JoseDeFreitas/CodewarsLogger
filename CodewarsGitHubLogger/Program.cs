06f83adf70a5acc1a9cca3cb3e04d19e4e05da14
Jose De Freitas <joselocotote26@gmail.com>
2021-09-15 20:29:01 -0400

    Replace fields with local variables

--- CodewarsGitHubLogger/Program.cs
+++ CodewarsGitHubLogger/Program.cs
@@ -10,11 +10,11 @@ namespace CodewarsGitHubLogger
     class Program
     {
         static HttpClient httpClient = new HttpClient();
-        static string codewarsUsername = "JoseDeFreitas"; // Environment.GetEnvironmentVariable("CODEWARS_USERNAME");
-        static string katasUrl = $"https://www.codewars.com/api/v1/users/{codewarsUsername}/code-challenges/completed";
 
         static async Task Main(string[] args)
         {
+            string codewarsUsername = "JoseDeFreitas"; // Environment.GetEnvironmentVariable("CODEWARS_USERNAME");
+            string katasUrl = $"https://www.codewars.com/api/v1/users/{codewarsUsername}/code-challenges/completed";
             string folderPath = "../Katas";
 
             Directory.CreateDirectory(folderPath);
