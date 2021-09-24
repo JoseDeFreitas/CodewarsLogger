5e7e57cf10a9546a27628740160445aef22e5636
Jose De Freitas <joselocotote26@gmail.com>
2021-09-23 21:23:42 -0400

    Write all languages with their extensions

--- CodewarsGitHubLogger/Program.cs
+++ CodewarsGitHubLogger/Program.cs
@@ -21,7 +21,21 @@ namespace CodewarsGitHubLogger
             string completedKatasUrl = $"https://www.codewars.com/api/v1/users/{codewarsUsername}/code-challenges/completed";
             string kataInfoUrl = "https://www.codewars.com/api/v1/code-challenges/";
             string mainFolderPath = "../Katas";
-            Dictionary<string, string> languageExtensions = new Dictionary<string, string>();
+            Dictionary<string, string> languagesExtensions = new Dictionary<string, string>() {
+                {"agda", "agda"}, {"bf", "b"}, {"c", "c"}, {"cmlf", "cmfl"},
+                {"clojure", "clj"}, {"cobol", "cob"}, {"coffeescript", "coffee"}, {"commonlisp", "lisp"},
+                {"coq", "coq"}, {"cplusplus", "cpp"}, {"crystal", "cr"}, {"csharp", "cs"},
+                {"dart", "dart"}, {"elixir", "ex"}, {"elm", "elm"}, {"erlang", "erl"},
+                {"factor", "factor"}, {"forth", "fth"}, {"fortran", "f"}, {"fsharp", "fs"},
+                {"go", "go"}, {"groovy", "groovy"}, {"haskell", "hs"}, {"haxe", "hx"},
+                {"idris", "idr"}, {"java", "java"}, {"javascript", "js"}, {"julia", "jl"},
+                {"kotlin", "kt"}, {"lean", "lean"}, {"lua", "lua"}, {"nasm", "nasm"},
+                {"nimrod", "nim"}, {"objective", "m"}, {"ocaml", "ml"}, {"pascal", "pas"},
+                {"perl", "pl"}, {"php", "php"}, {"powershell", "ps1"}, {"prolog", "pro"},
+                {"purescript", "purs"}, {"python", "py"}, {"r", "r"}, {"racket", "rkt"},
+                {"ruby", "rb"}, {"rust", "rs"}, {"scala", "scala"}, {"shell", "sh"},
+                {"sql", "sql"}, {"swift", "swift"}, {"typescript", "ts"}, {"vb", "vb"},
+            };
 
             Directory.CreateDirectory(mainFolderPath);
 
