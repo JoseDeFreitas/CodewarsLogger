1a315b815b2d4052956aa941662bc4ee796c3d27
José De Freitas <37962411+JoseDeFreitas@users.noreply.github.com>
2021-09-28 10:39:51 -0400

    Pass values as method arguments

--- CodewarsGitHubLogger/Program.cs
+++ CodewarsGitHubLogger/Program.cs
@@ -12,8 +12,8 @@ namespace CodewarsGitHubLogger
     class Program
     {
         static HttpClient httpClient = new HttpClient();
-        static string githubUsername = "JoseDeFreitas"; //Environment.GetEnvironmentVariable("USERNAME_GITHUB");
-        static string githubPassword = Environment.GetEnvironmentVariable("PASSWORD_GITHUB");
+        //static string githubUsername = Environment.GetEnvironmentVariable("USERNAME_GITHUB");
+        //static string githubPassword = Environment.GetEnvironmentVariable("PASSWORD_GITHUB");
         static int numberOfExceptions = 0;
         static List<string> idsOfExceptions = new List<string>();
         static Dictionary<string, string> languagesExtensions = new Dictionary<string, string>() {
@@ -39,20 +39,24 @@ namespace CodewarsGitHubLogger
             {"games", new List<string>()} // Equivalent to the "Puzzles" category
         };
 
-        static async Task Main(string[] createIndex)
+        static async Task Main(string[] args)
         {
             FirefoxOptions options = new FirefoxOptions();
             options.AddArgument("--headless");
             IWebDriver driver = new FirefoxDriver(options);
+            
+            string codewarsUsername = args[0];
+            string githubUsername = args[1];
+            string githubPassword = args[2];
 
-            string codewarsUsername = Environment.GetEnvironmentVariable("CODEWARS_USERNAME");
+            //string codewarsUsername = Environment.GetEnvironmentVariable("CODEWARS_USERNAME");
             string completedKatasUrl = $"https://www.codewars.com/api/v1/users/{codewarsUsername}/code-challenges/completed";
             string kataInfoUrl = "https://www.codewars.com/api/v1/code-challenges/";
             string mainFolderPath = "../Katas";
 
             Directory.CreateDirectory(mainFolderPath);
 
-            SigInToCodewars(driver);
+            SigInToCodewars(driver, githubUsername, githubPassword);
 
             // Response used only to get the total number of pages available
             Stream mainResponseJson = await httpClient.GetStreamAsync(completedKatasUrl);
@@ -95,7 +99,7 @@ namespace CodewarsGitHubLogger
                 }
             }
 
-            if (createIndex[0] == "-i")
+            if (args[3] == "-i")
                 await CreateIndexFileAsync();
 
             driver.Quit();
@@ -116,7 +120,7 @@ namespace CodewarsGitHubLogger
         /// </summary>
         /// <param name="driver">The Firefox driver initialised in the Main method.</param>
         /// <exception>When the driver can't connect to the Codewars website.</exception>
-        static void SigInToCodewars(IWebDriver driver)
+        static void SigInToCodewars(IWebDriver driver, string username, string password)
         {
             try
             {
@@ -131,8 +135,8 @@ namespace CodewarsGitHubLogger
             IWebElement siginForm = driver.FindElement(By.Id("new_user"));
             siginForm.FindElement(By.TagName("button")).Click();
 
-            driver.FindElement(By.Id("login_field")).SendKeys(githubUsername);
-            driver.FindElement(By.Id("password")).SendKeys(githubPassword);
+            driver.FindElement(By.Id("login_field")).SendKeys(username);
+            driver.FindElement(By.Id("password")).SendKeys(password);
             driver.FindElement(By.Name("commit")).Click();
         }
 
